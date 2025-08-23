f0c58b732d89e838a6cd646cbd4e0b7326f32c6f
Mikołaj <mikolajkocik@onet.pl>
2025-08-23 18:21:50 +0200

    update: endpoints

--- src/endpoints/image.py
+++ src/endpoints/image.py
@@ -1,11 +1,30 @@
-from fastapi import APIRouter, UploadFile
-from src.services.image_process_service import analyze_image
+from fastapi import APIRouter, UploadFile, File, HTTPException
+from src.services.image_process_service import analyze_image, recognize_image
 from src.models.image_insights import ImageInsights
 from io import BytesIO
+from typing import List
 
 router = APIRouter()
 
 @router.post("/analyze", response_model=ImageInsights)
-async def analyze(file: UploadFile):
-    image_stream = BytesIO(await file.read())
-    return analyze_image(image_stream)
+async def analyze(file: UploadFile = File(...)) -> ImageInsights:
+    try:
+        image_stream = BytesIO(await file.read())
+        return analyze_image(image_stream)
+    except RuntimeError as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Invalid image or request")
+    
+@router.post("/ocr", response_model=List[str])
+async def ocr(file: UploadFile = File(...)):
+    try:
+        image_stream = BytesIO(await file.read())
+        return recognize_image(image_stream)
+    except TimeoutError as e:
+        raise HTTPException(status_code=504, detail=str(e))
+    except RuntimeError as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Invalid image or request")
+    
